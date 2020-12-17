6b94059f0b71063c832f218ee8dd49d703f360b5
DIVYA SREE <divya220sree@gmail.com>
2020-12-17 12:31:40 +0100

    testing the drag and play section

--- src/handlers/handleProgress.js
+++ src/handlers/handleProgress.js
@@ -1,1 +1,8 @@
 'use strict';
+//  sets the time in tha video when the bar in dragged
+import { progressBar } from '../data.js';
+
+export function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+  }
--- src/handlers/scrub.js
+++ src/handlers/scrub.js
@@ -1,1 +1,6 @@
 'use strict';
+import { progress } from '../data.js';
+export function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+  }
