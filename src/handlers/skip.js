5ae2ccfb6287eb42648cbfada0d04b271f2b5031
Ketunze Brain <brainketunze@gmail.com>
2020-12-17 14:36:56 +0100

    handler/skip file

--- src/handlers/skip.js
+++ src/handlers/skip.js
@@ -1,1 +1,15 @@
 'use strict';
+
+/**
+ * play a video when the user press the skip button , which are connected to video files 
+ * @param {Event} event - triggered whenever a user press one of the skip button connected to the video files. 
+ */
+import {video} from '../data'; 
+
+
+export function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+   }
+   
+   const player = document.querySelector('.player');
+   const skipButtons = player.querySelectorAll('[data-skip]');
