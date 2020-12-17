6fb393be6f774600b6391ffc6317ae054e375b2a
KrystynaMil <kristina-manager@hotmail.com>
2020-12-17 13:43:13 +0100

    modified import export

--- src/init.js
+++ src/init.js
@@ -1,3 +1,6 @@
 'use strict';
 
 // import all listener files so they can attach to the DOM
+
+import './listeners/click-togglePlay';
+import  './listeners/play-pause-updateButton.js';
--- src/listeners/click-togglePlay.js
+++ src/listeners/click-togglePlay.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import {togglePlay} from '../handlers/togglePlay.js';
-
+import {video, toggle} from '../data';
 
 video.addEventListener('click', togglePlay);
 toggle.addEventListener('click', togglePlay);
