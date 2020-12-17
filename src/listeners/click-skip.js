14edbae7b285aad088729cdb18a6011fcfe2cbb2
Ketunze Brain <brainketunze@gmail.com>
2020-12-17 14:35:01 +0100

    click-skip.js

--- src/listeners/click-skip.js
+++ src/listeners/click-skip.js
@@ -1,3 +1,10 @@
 'use strict';
 
-import {} from '';
+import {skip} from '../handlers/skip.js';
+
+/**
+ * @name user input
+ * calls the Handler when a user pressed the skip update 
+ * */
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
